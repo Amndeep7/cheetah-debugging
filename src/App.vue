99980488ddb77a070f32f5e5cf7e475cb5c76ce8
Amndeep Singh Mann <amann@mitre.org>
2020-05-20 19:42:55 -0400

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>Cheetah header test</div>
+    <div class="wrapper">
+      <c-grid ref="grid" :data="data">
+        <template slot="layout-header">
+          <c-grid-layout-row>
+            <c-grid-header :width="200" :header-field="'asdf'" :header-action="'check'" @changed-header-value="onChange">
+              {{'title ' + 'asdf'}}
+            </c-grid-header>
+          </c-grid-layout-row>
+          <c-grid-layout-row>
+            <c-grid-header :width="200" :header-field="'asdf'" :header-action="'check'" @changed-header-value="onChange">
+              {{'sum ' + data.reduce((acc,cur) => acc + cur['asdf'], 0)}}
+            </c-grid-header>
+          </c-grid-layout-row>
+          <c-grid-layout-row>
+            <c-grid-header :width="200" :header-field="'asdf'" :header-action="'check'" @changed-header-value="onChange">
+              {{'min ' + Math.min(...(data.map(d=>d.asdf)))}}
+            </c-grid-header>
+          </c-grid-layout-row>
+        </template>
+        <template slot="layout-body">
+          <c-grid-layout-row>
+            <c-grid-column :field="'asdf'" :width="200"/>
+          </c-grid-layout-row>
+        </template>
+      </c-grid>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// eslint-disable
+import * as cGridAll from 'vue-cheetah-grid'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ...cGridAll,
+  },
+  data() {
+    return {
+      data: (new Array(10)).fill(0).map(i => ({ 'asdf': Math.random() })),
+    }
+  },
+  methods: {
+    onChange() {
+      this.data = (new Array(10)).fill(0).map(i => ({ 'asdf': Math.random() }));
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.wrapper {
+  width: 250px;
+  height: 550px;
 }
 </style>
